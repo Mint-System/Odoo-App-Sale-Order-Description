1353f3246895779a67e7a25378528abec6c207cf
Janik von Rotz <login@janikvonrotz.ch>
2020-06-22 12:49:38 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Odoo App: Sale Order Description
+
+Customize the sale order description field.
---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Order Description",
+    'summary': """
+        Customize the sale order description field.
+    """,
+
+    'description': """
+        Customize the sale order description field.
+    """, 
+    'author': "Mint System",
+    'website': "https://www.mint-system.ch",
+    'category': 'Sales',
+    'version': '0.1.0',
+    'depends': ['sale'],
+    'data': [
+        'views/raw.xml',
+    ],
+}
