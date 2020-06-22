87f74d0fc65344c02f8010ed643637089d0d38f8
Janik von Rotz <login@janikvonrotz.ch>
2020-06-22 20:59:15 +0200

    set html widget for line and note

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
     'author': "Mint System",
     'website': "https://www.mint-system.ch",
     'category': 'Sales',
-    'version': '0.1.0',
+    'version': '0.1.1',
     'depends': ['sale'],
     'data': [
         'views/raw.xml',
